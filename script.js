d4d722c9ded78dc086a0016ba6e083ab20181fed
Anna Malko <92226604+AnnMalko@users.noreply.github.com>
2026-01-13 14:03:07 -0500

    Update Christmas countdown date to 2026

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function christmasCountDown() {
-	const christmasDate = new Date('December 25,2025 00:00');
+	const christmasDate = new Date('December 25,2026 00:00');
 	const now = new Date();
 	const diff = christmasDate - now;
 
